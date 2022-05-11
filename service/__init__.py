af7ff9c0b07d7d93c2d47f1dc02b95e98c52495b
kimnamhyeon <knh306@naver.com>
2022-05-11 19:39:13 +0900

    __init__.py, test_api.sh, list.html updated

--- service/__init__.py
+++ service/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template
 from flask_cors import CORS
+from bson import json_util
 import datetime
 import os
 from da_design_server.src import mylogger, myconfig, mydb
@@ -24,6 +25,17 @@ def web_main():
 def web_help():
     return render_template("help.html")
 
+@app.route('/list')
+def web_list():
+    ret = get_list_topk(10)
+    ret_json = json_util.dumps(ret, ensure_ascii=False)
+    return render_template("list.html",
+        list_info=ret_json)
+
+def get_list_topk(topk):
+    today_date = datetime.datetime.now()
+    result = db.get_company_value_of_date(today_date, topk)
+    return result
 @app.route('/api-list', methods=["POST"])
 def api_list():
     top_k = request.json.get('top_k')
@@ -32,8 +44,7 @@ def api_list():
     ret = {"result": None, "msg": ""}
     if top_k:
         top_k = int(top_k)
-        today_date = datetime.datetime.now()
-        result = db.get_company_value_of_date(today_date, top_k)
+        result = get_list_topk(top_k)
         if result:
             ret["result"] = result
         else:
@@ -44,3 +55,21 @@ def api_list():
     logger.info('< API:list with {}'.format(ret))
     return ret
 
+@app.route('/api-predict', methods=["POST"])
+def api_predict():
+    company_name = request.json.get('company_name')
+    logger.info('> API:predict with {}'.format(company_name))
+
+    ret = {"result": None, "msg": ""}
+    if company_name:
+        result = db.get_predicted_company_stock(company_name, logger)
+        if result:
+            ret["result"] = result
+            ret['msg'] = company_name
+        else:
+            ret['msg'] = '결과값 생성에 실패하였습니다.'
+    else:
+        ret['msg'] = 'company_name 값이 주어져있지 않습니다.'
+
+    logger.info('< API:list with {}'.format(ret))
+    return ret
--- service/test_api.sh
+++ service/test_api.sh
@@ -1,7 +1,11 @@
 if [ $# -lt 1 ]; then
-	echo 'give 1 argument'
+        echo 'give 1 argument'
 else
-	if [ $1 = 'list' ]; then
-		curl -H "Content-type: application/json" -X POST -d '{"top_k":"10"}' http://아이피주소:10102/api-list >> ret
-	fi
+        if [ $1 = 'list' ]; then
+                curl -H "Content-type: application/json" -X POST -d '{"top_k":"10"}' http://아이피주소:10101/api-list >> ret
+        fi
+        if [ $1 = 'predict' ]; then
+                curl -H "Content-type: application/json" -X POST -d '{"company_name":"삼성전자"}' http://아이피주소:10101/api-predict >> ret
+        fi
 fi
+
